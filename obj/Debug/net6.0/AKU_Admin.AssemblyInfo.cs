1232fc7c81947037b3096ba1e512f11974de3c61
AmmadRaza1993 <ammadsiddiqui136@gmail.com>
2024-02-01 19:53:48 +0500

    .

--- obj/Debug/net6.0/AKU_Admin.AssemblyInfo.cs
+++ obj/Debug/net6.0/AKU_Admin.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MamjiAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af20e6506048f779e4593afad3a8d4a14905727")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MamjiAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MamjiAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
